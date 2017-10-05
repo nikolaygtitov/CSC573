ee30d2a7880f301dc7374a2956504270ab366f98
Nikolay Titov <nikolay.titov@nokia.com>
2017-10-05 00:59:53 -0400

    Registration Server data structure implementation for registering peers

--- Project_1/ngtitov_peers.py
+++ Project_1/ngtitov_peers.py
@@ -1,18 +1,52 @@
 # ngtitov_peers.py
 
+# The Application Layer Protocol for peer-to-RS communication of P2P-DI/1.0 is
+# defined as follows:
+'''
+-------------------------------------
+ Method | Protocol name and version |
+-------------------------------------
+ Host:  |           IPv4            |
+-------------------------------------
+ Port:  |           xxxxx           |
+-------------------------------------
+  OS:   |           System          |
+-------------------------------------
+ Date:  | Yr-Mo-Day Hr-Min-Sec-Msec |
+-------------------------------------
+ EOP (End of Protocol)              |
+-------------------------------------
+'''
 # Import Python's libraries
+import sys
+import platform
+import datetime
 from socket import *
 from random import randint
 
 # Initialization of constants
 SERVER_IP = 'localhost'
 SERVER_PORT = 65423
-PROTOCOL_REG = 'REGISTER PEER P2P-DI/1.0'
-PROTOCOL_HOST = 'HOST: {}'
-PROTOCOL_PORT = 'PORT: {}'
+PROTOCOL_METHOD = '{} ' + 'P2P-DI/1.0\n'
+PROTOCOL_RFC_HOST_SERVER = 'Host: {}\n'
+PROTOCOL_RFC_PORT_SERVER = 'Port: {}\n'
+PROTOCOL_OS = 'OS: {}\n'
+PROTOCOL_DATE = 'Date: {}\n'
+PROTOCOL_EOP = 'EOP'
 # Generate a random port number to which RFC server of this peer is listening
 # Ports must be in the range [65400-65500] since VCL/EOS blocks all other ports
 RFC_PORT = randint(65400, 65500)
+
+
+def encapsulate_data_protocol(request):
+    _method = PROTOCOL_METHOD.format(request)
+    _rfc_host_server = PROTOCOL_RFC_HOST_SERVER.format(rfc_host_server)
+    _rfc_port_server = PROTOCOL_RFC_PORT_SERVER.format(rfc_port_server)
+    _os = PROTOCOL_OS.format(platform.platform())
+    _date = PROTOCOL_DATE.format(datetime.datetime.now())
+    _protocol = _method + _rfc_host_server + _rfc_port_server + _os + _date +\
+        PROTOCOL_EOP
+    return _protocol
 
 # Create a TCP server welcoming socket and bind it to a well-known port
 try:
@@ -29,7 +63,7 @@ except error, (value, message):
 
 # Determine IP address and port number associated with the RFC server welcoming
 # pocket
-rfc_ip_server = getaddrinfo(
+rfc_host_server = getaddrinfo(
     gethostname(), rfc_socket.getsockname()[1], AF_INET, SOCK_STREAM)[0][-1][0]
 rfc_port_server = getaddrinfo(
     gethostname(), rfc_socket.getsockname()[1], AF_INET, SOCK_STREAM)[0][-1][1]
@@ -45,11 +79,10 @@ except AssertionError, e:
 try:
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect((SERVER_IP, SERVER_PORT))
-    rs_request_message = PROTOCOL_REG + '\n' + PROTOCOL_HOST.format(
-        rfc_ip_server) + '\n' + PROTOCOL_PORT.format(rfc_port_server)
+    rs_request_message = encapsulate_data_protocol('REGISTER')
     client_socket.send(rs_request_message.encode())
     rs_response_message = client_socket.recv(1024)
-    print rs_request_message.decode()
+    print rs_response_message.decode()
     client_socket.close()
     del client_socket
 except (error, herror, gaierror, timeout), (value, message):
--- Project_1/ngtitov_registration_server.py
+++ Project_1/ngtitov_registration_server.py
@@ -1,11 +1,106 @@
 # ngtitov_registration_server.py
+# The Application Layer Protocol for peer-to-RS communication of P2P-DI/1.0 is
+# defined as follows:
+'''
+-------------------------------------
+ Method | Protocol name and version |
+-------------------------------------
+ Host:  |           IPv4            |
+-------------------------------------
+ Port:  |           xxxxx           |
+-------------------------------------
+  OS:   |           System          |
+-------------------------------------
+ Date:  | Yr-Mo-Day Hr-Min-Sec-Msec |
+-------------------------------------
+ EOP (End of Protocol)              |
+-------------------------------------
+'''
 
 # Import Python's libraries
 import sys
+import datetime
+import time
 from socket import *
 
 # Initialization of constants
 SERVER_PORT = 65423
+TTL = 7200
+THIRTY_DAYS = 2592000
+PROTOCOL_EOP = 'EOP'
+
+
+dict_peers = {}
+
+
+def extract_data_protocol():
+    _data_list = received_data.decode().split()
+    _method = _data_list[0]
+    _host = _data_list[_data_list.index('Host:') + 1]
+    _port = _data_list[_data_list.index('Port:') + 1]
+    return _method, _host, _port
+
+
+def get_cookie():
+    for _cookie, _peer in dict_peers.iteritems():
+        if _peer.hostname == host:
+            try:
+                assert _cookie == _peer.cookie, \
+                    'Cookie for peer {} does not match!'.format(host)
+                return _cookie
+            except AssertionError, _e:
+                sys.exit(_e)
+    return None
+
+
+def execute_request():
+    if method == 'REGISTER':
+        if cookie is None:
+            peer = Peer(host, len(dict_peers), True, port,
+                        datetime.datetime.now())
+            dict_peers[len(dict_peers)] = peer
+        else:
+            peer = dict_peers.get(cookie)
+            peer.register_update(port)
+    elif method == 'LEAVE':
+        pass
+    elif method == 'PQUERY':
+        pass
+    elif method == 'KEEPALIVE':
+        pass
+    else:
+        pass
+
+
+class Peer:
+    def __init__(self, _hostname, _cookie, _flag, _port, _reg_date, _ttl=TTL):
+        self.hostname = _hostname
+        self.cookie = _cookie
+        self.flag = _flag
+        self.ttl = _ttl
+        self.port = _port
+        self.reg_date = _reg_date
+        self.reg_times = [time.time()]
+
+    def register_update(self, _port):
+        self.flag = True
+        self.ttl = TTL
+        self.port = _port
+        _reg_time = time.time()
+        self.reg_times.append(_reg_time)
+        for reg_time in self.reg_times:
+            if _reg_time - reg_time > THIRTY_DAYS:
+                self.reg_times.remove(reg_time)
+            else:
+                return
+        return
+
+    def update(self):
+        _time = time.time() - self.reg_times[-1]
+        self.ttl = self.ttl - _time
+        self.flag = True if self.ttl > 0 else False
+        return
+
 
 # Create a TCP server welcoming socket and bind it to a well-known port
 try:
@@ -25,9 +120,26 @@ except error, (value, message):
 while True:
     # Wait on accept and create new socket
     connection_socket, address = server_socket.accept()
-    # Read peers data and request from socket
+    # Read peer's request data from socket
     received_data = connection_socket.recv(1024)
-    print received_data.decode()
+    try:
+        assert PROTOCOL_EOP in received_data.decode(),\
+            'Did not receive all the data yet.. Wait..'
+    except AssertionError, e:
+        print e
+        while PROTOCOL_EOP not in received_data.decode():
+            received_data += connection_socket.recv(1024)
+    method, host, port = extract_data_protocol()
+    cookie = get_cookie()
+    execute_request()
+    for x, y in dict_peers.iteritems():
+        y.update()
+        print x, ' ==> ', 'Hostname: {} '.format(y.hostname), \
+            'Cookie: {} '.format(y.cookie), 'Flag: {} '.format(y.flag), \
+            'TTL: {} '.format(y.ttl), 'Port: {} '.format(y.port), \
+            'Most Recent Registration Date: {} '.format(y.reg_date), \
+            'Times host been registered for last 30 days: {} '.format(len(
+                y.reg_times))
     connection_socket.send("OK 0".encode())
     connection_socket.close()
     del connection_socket
